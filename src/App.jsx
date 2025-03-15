8e842280807f776d15b65559a7248541bbedccbc
Priyanshu Singh <singhpriyanshu086@gmail.com>
2025-03-15 21:53:55 +0530

    `Added NavBar`

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,54 @@
 import "./index.css";
 
-function App() {
+const navBar = () => {
   return (
     <>
-      <div className="navBar">
-        <div className="bg-gray-400">
-          <ul className="flex justify-end px-11">
-            <li><a href="#">User</a></li>
-            <li><a href="#">SignIn</a></li>
-          </ul>
+      <div className="navbar bg-base-300 shadow-sm">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">daisyUI</a>
+        </div>
+        <div className="flex gap-2">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered w-24 md:w-auto"
+          />
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-      <h1 className="bg-black text-white"> Hello World</h1>
+      <h1 className="font-bold text-3xl">Hello World</h1>
     </>
   );
-}
+};
 
-export default App;
+export default navBar;
